c0de0763c8e04528164bfef3f82dd9b364451742
Mohammed-Masood-SE <mohammed.masood.se@gmail.com>
2024-04-21 11:10:41 +0400

    Initial Project Setup

---
+++ src/Screens/Home/Home.js
@@ -0,0 +1,12 @@
+import styles from "./Home.module.css"
+
+function Home() {
+    return (
+      <div className={styles.homeWrapper}>
+        
+      </div>
+    );
+  }
+  
+  export default Home;
+  
